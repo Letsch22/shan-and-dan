5f1b77e105dde96035844a263521a9f618ca68b0
Daniel Letscher <dpletscher@outlook.com>
2022-03-14 22:17:45 -0700

    Close touch menu on selection

--- public/js/custom.js
+++ public/js/custom.js
@@ -8,6 +8,17 @@ for (item of navItems) {
             top: document.getElementById(e.target.dataset.target).getBoundingClientRect().top +
                 window.scrollY - 20
         });
+        // Get all "navbar-burger" elements
+        const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+        // Check if there are any navbar burgers
+        if ($navbarBurgers.length > 0) {
+
+          $navbarBurgers.forEach( el => {
+            el.classList.toggle('is-active');
+          });
+        }
+        document.getElementById('navbar-items').classList.toggle('is-active');
     });
 }
 
